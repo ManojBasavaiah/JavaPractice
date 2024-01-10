1fcab1e82d9cb678c2de809de8aba58d4fb075ff
ManojBasavaiah <manojbasavaiah01@gmail.com>
2024-01-10 07:31:11 +0530

    commit all changes
    Signed-off-by: ManojBasavaiah <manojbasavaiah01@gmail.com>

--- src/ExtractDigits.java
+++ src/ExtractDigits.java
@@ -1,9 +1,9 @@
 class ExtractDigits {
     public static String extractDigits(int n) {
-        int count=0;
-        while(n!=0){
-            int lastdigit=n%10;
-            n=n/10;
+        int count = 0;
+        while (n != 0) {
+            int lastdigit = n % 10;
+            n = n / 10;
             count++;
         }
         return String.valueOf(count);
