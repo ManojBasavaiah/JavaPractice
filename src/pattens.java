925fd0387dcfa5c672e009583ee25c35b55516ad
ManojBasavaiah <manojbasavaiah01@gmail.com>
2024-01-11 07:17:34 +0530

    commit all changes
    Signed-off-by: ManojBasavaiah <manojbasavaiah01@gmail.com>

--- src/pattens.java
+++ src/pattens.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class pattens {
     public static void main(String[] args) {
+        int t = 2;
         System.out.println("Enter n value: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+        for (int k = 0; k < t; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            System.out.println("");
         }
-    }
 
+    }
 }
